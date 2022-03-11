2cdd2e7f78ab55285f2dd5321e4149c62cc5fa36
CalebMatherne <ctm042@email.latech.edu>
2022-03-11 17:09:49 -0600

    add test.py for testing purposes

---
+++ test.py
@@ -0,0 +1,11 @@
+# just testing to see if everyone can pull and push updates
+# type some stuff in this file and push it to make sure it's working
+
+# PUSH (update the online repo with your changes)
+# cd into the cloned repo with git bash
+# git add test.py 
+# git commit -m "commit description"
+# git push
+
+# PULL (update your local files)
+# git pull
