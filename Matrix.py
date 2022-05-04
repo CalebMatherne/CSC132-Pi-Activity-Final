740a0934e7d4a25dfa57b492674e22a611abd6e5
CalebMatherne <ctm042@email.latech.edu>
2022-05-03 20:26:53 -0500

    Added file structure and Updated Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -2,30 +2,29 @@ import time
 import random
 import pygame
 import RPi.GPIO
+import os, os.path
 
 
 # GPIO pin assignments.
-HUB75E_R1 = 17 # was 14
-HUB75E_G1 = 18 # was 15
-HUB75E_B1 = 22 # was 18
-HUB75E_R2 = 23
-HUB75E_G2 = 24
-HUB75E_B2 = 25
-# HUB75E_E = 8
-HUB75E_A = 27 # was 7
-HUB75E_B = 6 # was 12
-HUB75E_C = 9 # was 16
-#HUB75E_D = 20
-HUB75E_CLK = 3 # was 21
-HUB75E_LAT = 4 # was 26
-HUB75E_OE = 2 # was 19
+pin_R1 = 17
+pin_G1 = 18
+pin_B1 = 22
+pin_R2 = 23
+pin_G2 = 24
+pin_B2 = 25
+pin_A = 27
+pin_B = 6
+pin_C = 9
+pin_Clock = 3 #SCL
+pin_Latch = 4
+pin_OE = 2
 
 # Display properties.
 RED = 0
 GREEN = 1
 BLUE = 2
-FRAME_REPEAT = 50
-DISPLAY_FRAMES = 4
+FRAME_REPEAT = 50                                        #set from animation file?
+DISPLAY_FRAMES = 4                                       #will change with length of animation
 DISPLAY_COLS = 32
 DISPLAY_ROWS = 16
 
@@ -36,22 +35,20 @@ pygame.init()
 # Configure GPIO pins.
 RPi.GPIO.setwarnings(False)
 RPi.GPIO.setmode(RPi.GPIO.BCM)
-RPi.GPIO.setup(HUB75E_R1, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_G1, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_B1, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_R2, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_G2, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_B2, RPi.GPIO.OUT, initial=0)
-# RPi.GPIO.setup(HUB75E_E, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_A, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_B, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_C, RPi.GPIO.OUT, initial=0)
-#RPi.GPIO.setup(HUB75E_D, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_CLK, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_LAT, RPi.GPIO.OUT, initial=0)
-RPi.GPIO.setup(HUB75E_OE, RPi.GPIO.OUT, initial=1)
+RPi.GPIO.setup(pin_R1, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_G1, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_B1, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_R2, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_G2, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_B2, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_A, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_B, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_C, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_Clock, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_Latch, RPi.GPIO.OUT, initial=0)
+RPi.GPIO.setup(pin_OE, RPi.GPIO.OUT, initial=1)
 
-# Load animation image files.
+# Load animation image files.                            #change to look for images in right folder
 FrameImage = []
 FrameImage.append(pygame.image.load("LaTechLogo1.png"))
 FrameImage.append(pygame.image.load("LaTechLogo2.png"))
@@ -59,7 +56,10 @@ FrameImage.append(pygame.image.load("LaTechLogo3.png"))
 FrameImage.append(pygame.image.load("LaTechLogo4.png"))
 FrameImage.append(pygame.image.load("LaTechLogo5.png"))
 
-# Load a display frames from images.
+for pict in os.listdir("."):
+   print(pict)
+
+# Load a display frames from images.                     #attempt to include intermediate colors
 DisplayImage = []
 for Frame in range(DISPLAY_FRAMES):
    DisplayImage.append([])
@@ -88,9 +88,9 @@ while True:
    # Update LED Matrix.
    for Row in range(int(DISPLAY_ROWS / 2)):
       # Select row to dispaly.
-      RPi.GPIO.output(HUB75E_A, Row & 1)
-      RPi.GPIO.output(HUB75E_B, Row & 2)
-      RPi.GPIO.output(HUB75E_C, Row & 4)
+      RPi.GPIO.output(pin_A, Row & 1)
+      RPi.GPIO.output(pin_B, Row & 2)
+      RPi.GPIO.output(pin_C, Row & 4)
 #      RPi.GPIO.output(HUB75E_D, Row & 8)
 #      RPi.GPIO.output(HUB75E_E, Row & 16)
 
@@ -99,23 +99,23 @@ while True:
          SelRow = 0
       for Col in range(DISPLAY_COLS):
          # Load bits into top row set.
-         RPi.GPIO.output(HUB75E_R1, DisplayImage[Frame][SelRow][Col][RED])
-         RPi.GPIO.output(HUB75E_G1, DisplayImage[Frame][SelRow][Col][GREEN])
-         RPi.GPIO.output(HUB75E_B1, DisplayImage[Frame][SelRow][Col][BLUE])
+         RPi.GPIO.output(pin_R1, DisplayImage[Frame][SelRow][Col][RED])
+         RPi.GPIO.output(pin_G1, DisplayImage[Frame][SelRow][Col][GREEN])
+         RPi.GPIO.output(pin_B1, DisplayImage[Frame][SelRow][Col][BLUE])
 
          # Load bits into bottom row set.
-         RPi.GPIO.output(HUB75E_R2, DisplayImage[Frame][SelRow + 8][Col][RED])
-         RPi.GPIO.output(HUB75E_G2, DisplayImage[Frame][SelRow + 8][Col][GREEN])
-         RPi.GPIO.output(HUB75E_B2, DisplayImage[Frame][SelRow + 8][Col][BLUE])
+         RPi.GPIO.output(pin_R2, DisplayImage[Frame][SelRow + 8][Col][RED])
+         RPi.GPIO.output(pin_G2, DisplayImage[Frame][SelRow + 8][Col][GREEN])
+         RPi.GPIO.output(pin_B2, DisplayImage[Frame][SelRow + 8][Col][BLUE])
 
          # While clocking in new bit data.
          # Refresh existing display data on the current output row.
-         RPi.GPIO.output(HUB75E_OE, 0)
-         RPi.GPIO.output(HUB75E_CLK, 1)
-         RPi.GPIO.output(HUB75E_OE, 1)
-         RPi.GPIO.output(HUB75E_CLK, 0)
+         RPi.GPIO.output(pin_OE, 0)
+         RPi.GPIO.output(pin_Clock, 1)
+         RPi.GPIO.output(pin_OE, 1)
+         RPi.GPIO.output(pin_Clock, 0)
 
       # When a pair of rows of display bits has been loaded.
       # Latch the data into the output buffer.
-      RPi.GPIO.output(HUB75E_LAT, 1)
-      RPi.GPIO.output(HUB75E_LAT, 0)
+      RPi.GPIO.output(pin_Latch, 1)
+      RPi.GPIO.output(pin_Latch, 0)
